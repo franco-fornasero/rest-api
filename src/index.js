6c727a1352f5197295be9c3dc6ac164512b5fc15
Franco Fornasero <franco.fornasero@gmail.com>
2022-04-01 23:18:05 -0300

    Avances getMessage

--- src/index.js
+++ src/index.js
@@ -14,32 +14,38 @@ app.use(express.urlencoded({extended : false}));
 app.use(express.json());
 
 // routes
-//app.use(require('./routes/index'));
 app.post('/api/topsecret', async (req, res) => {
     content = req.body;
-    
     // faltan chequear que los datos estén bien
-   
-    limpiarRequest(content);
-    const jsonwaolf = await GetLocation(distances);
+    distancias = obtenerDistancias(content);
+    mensajes = obtenerMensajes(content);
+    getMessage(mensajes);
+    //console.log(mensajes);
+    const jsonwaolf = await GetLocation(distancias);
     await res.send(jsonwaolf);
-
-    
 });
+
+
 // starting the server
 app.listen(app.get('port'), () => {
     console.log(`Server on port ${3000}`);
 });
 
-let distances = [];
-function limpiarRequest(content){
+
+function obtenerDistancias(content){
+    let distances = [];
     content.satellites.forEach(satellite => {
         distances = [...distances, satellite.distance];
     });
-    //console.log(distances);
-    
+    return distances;    
 }
 
+function obtenerMensajes(content){
+    let messages = content.satellites.map(satellite => {
+        return satellite.message;
+    });
+    return messages;
+}
 
 coordKenobi = {
                 'x':'-500',
@@ -57,20 +63,16 @@ coordSato = {
 
 async function GetLocation(distances){
     const sistemaEcuaciones = `{(${distances[0]})^2 = (x - ${coordKenobi.x})^2 + (y - ${coordKenobi.y})^2,(${distances[1]})^2 = (x - ${coordSkywalker.x})^2 + (y - ${coordSkywalker.y})^2, (${distances[2]})^2 = (x - ${coordSato.x})^2 + (y - ${coordSato.y})^2}`;
-    //console.log(sistemaEcuaciones);
     const sistemaEcuacionesURL = encodeURIComponent(sistemaEcuaciones);
-    //console.log(sistemaEcuacionesURL);
     const reqWolfram = `https://api.wolframalpha.com/v2/query?input=${sistemaEcuacionesURL}&format=plaintext&output=JSON&appid=HWHT7U-7QKUET5T4K`;
     const responseWolfram = await fetch(reqWolfram);
     const responseWolframJSON = await responseWolfram.json();
     let coordenadas = []
     responseWolframJSON.queryresult.pods.forEach(a => {
-        //console.log(a.title, a.id);
         if (a.title == 'Solution' && a.id == 'Solution'){
             a.subpods.forEach(b => {
                 coordenadas = [...coordenadas, b.plaintext];
             });
-            //console.log(resultado);
             return coordenadas;
         }
     });
@@ -79,15 +81,59 @@ async function GetLocation(distances){
     }else {
         coordenadas.forEach( a => {
             let cadenas = a.split(",");
-            //console.log(cadenas);
             posiciones = cadenas.map(b => {
                 return parseFloat(b.substring(5));
             });
-            
-            console.log(posiciones);
         })
         return posiciones;
     }
 }
 
+function getMessage(messages){
+    console.log('funcion');
+    /*const min = Math.min(messages[0].length, messages[1].length, messages[2].length);
+    const max = Math.max(messages[0].length, messages[1].length, messages[2].length);*/
+    /*let iKenobi = messages[0].length;
+    let iSkywalker = messages[1].length;
+    let iSato = messages[2].length;*/
+    let corte = false;
+    let c = 1;
+    let msg = '';
+    while(corte == false){
+        //break;
+        let cadenas = [
+            messages[0][messages[0].length - c],
+            messages[1][messages[1].length - c],
+            messages[2][messages[2].length - c]
+        ]
+        console.log(cadenas);
+        const palabra = retornarNoVacia(cadenas);
+        console.log(palabra);
+        if (palabra == ''){
+            corte = true;
+        }
+        else{
+            if (c = messages[0].length){
+                corte = true
+            }
+            else {
+                c++;
+                msg+= ` ${palabra}`; 
+            }
+          
+        }  
+    }
+    console.log(msg);
+    
+}
 
+function retornarNoVacia(cadenas){
+    let palabra = ''
+    //map devuelve un array, arreglar
+    palabra = cadenas.map(mensaje => {
+       if (mensaje != ''){
+           return mensaje;
+       }
+    });
+    return palabra;
+}
