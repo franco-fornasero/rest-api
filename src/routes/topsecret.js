def2c300a56ed0366db69e2a3736a1af2287fe95
Franco Fornasero <franco.fornasero@gmail.com>
2022-04-06 13:54:17 -0300

    Avances topsecret_split

--- src/routes/topsecret.js
+++ src/routes/topsecret.js
@@ -2,14 +2,14 @@ const express = require('express');
 const { GetLocation, obtenerDistancias } = require('../controllers/GetLocation');
 const { GetMessage, obtenerMensajes} = require('../controllers/GetMessage')
 const router = express.Router();
-var bodyParser = require('body-parser')
-var jsonParser = bodyParser.json()
+const bodyParser = require('body-parser')
+const jsonParser = bodyParser.json()
 
 
 router.post('/topsecret', jsonParser,async (req, res) => {
-    content = req.body;
-    distancias = obtenerDistancias(content);
-    mensajes = obtenerMensajes(content);
+    const content = req.body;
+    const distancias = obtenerDistancias(content);
+    const mensajes = obtenerMensajes(content);
     const mensaje = GetMessage(mensajes);
     const posiciones = await GetLocation(distancias);
     let response;
--- src/routes/topsecret_split.js
+++ src/routes/topsecret_split.js
@@ -1,17 +1,32 @@
 const express = require('express');
+const fs = require('fs');
 const { GetLocation, obtenerDistancias } = require('../controllers/GetLocation');
-const { GetMessage, obtenerMensajes} = require('../controllers/GetMessage')
+const { GetMessage, obtenerMensajes} = require('../controllers/GetMessage');
 const router = express.Router();
-const bodyParser = require('body-parser')
-const jsonParser = bodyParser.json()
-const file = require('../data/satellites.json')
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+const file = require('../data/satellites.json');
 
-
-router.get('/topsecret_split', jsonParser,async (req, res) => {
-    
-    res.send(file);
+router.post('/topsecret_split/:satellite', jsonParser,async (req, res) => {
+    const {satellite} = req.params;
+    if (satellite == 'kenobi' || satellite == 'skywalker' || satellite == 'sato'){
+        let content = req.body;
+        file.forEach(reg => {
+            if (reg.satellite == satellite){
+                reg.message = content.satellite;
+                reg.distance = content.distance;
+            }
+        })
+        /*content["satellite"] = satellite;
+        file.push(content);*/
+        fs.writeFile('src/data/satellites.json', JSON.stringify(file), () =>  console.log('pa q quiere esto'));
+        res.send('ok');
+    }
+    //res.json(file);
 });
 
-
+router.get('/topsecret_split/:satellite', jsonParser,async (req, res) => {
+    res.send(file);
+});
 
 module.exports = router;
