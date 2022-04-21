1578fb7cc97b878783aa6b12714ed1ffd1e159c5
Franco Fornasero <franco.fornasero@gmail.com>
2022-04-21 09:17:10 -0300

    Fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const port = process.env.PORT || 3000;
+const res = require('express/lib/response');
 const app = require('./app');
 
 app.listen(port, () => {
     console.log(`Server on port ${port}`);
+    res.send('ok');
 });
